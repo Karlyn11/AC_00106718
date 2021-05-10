bcd198783a0674693924258a2b66a32b8fa6f9b8
Karlyn <00106718@gmail.com>
2021-05-10 15:42:07 -0600

    Ejercicio 1 corregido

--- Labo3/Ejercicio1.asm
+++ Labo3/Ejercicio1.asm
@@ -1,16 +1,27 @@
-org     100h
+org 	100h
 
-   section .text
+	section	.text
 
-MOV AX,0d
-ADD AX,0d
-ADD AX,1d
-ADD AX,0d
-ADD AX,6d
-ADD AX,7d
-ADD AX,1d
-ADD AX,8d
-MOV BX,8d
-DIV BX
-Mov [20Ah],AX
-int 20h
+
+    MOV byte [210h],0d
+    MOV byte [211h],0d
+    MOV byte [212h],1d
+    MOV byte [213h],0d
+    MOV byte [214h],6d
+    MOV byte [215h],7d
+    MOV byte [216h],1d
+    MOV byte [217h],8d
+    MOV AX,0d
+    MOV SI,0d
+    MOV BX,8d
+    jmp iterando
+    iterando:
+    CMP SI,8d
+    JE exit
+    ADD AL,  [210h + SI]
+    INC SI
+    jmp iterando
+    exit:
+    DIV BX
+    mov [20Ah],AX
+    int 20h
